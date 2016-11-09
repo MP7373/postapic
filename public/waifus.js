2f7cd6c2827297b736f5d3fa7ef9261780374cde
MP7373 <martinpayne73@gmail.com>
2016-11-09 17:31:52 -0500

    Added delete funtionallity

--- public/waifus.js
+++ public/waifus.js
@@ -1,11 +1,23 @@
 var app = angular.module("waifus", []);
 
 app.controller("waifusController", function ($scope, $http) {
-	$http.get("/waifus").then(function(response) {
-		$scope.waifus = response.data;
-	})
+	updateWaifusFeed();
 
 	$scope.submitWaifu = function () {
 		$http.post("/waifus", {newWaifuName: $scope.newWaifuName, newWaifuPic: $scope.newWaifuPic});
-	};
+		$scope.newWaifuName = "";
+		$scope.newWaifuPic = "";
+		updateWaifusFeed();
+	}
+	
+	$scope.deleteWaifu = function (waifu) {
+		$http.put("/waifus/delete", {waifuToDeleteId: waifu._id});
+		updateWaifusFeed();
+	}
+
+	function updateWaifusFeed () {
+		$http.get("/waifus").then(function(response) {
+			$scope.waifus = response.data;
+		});
+	}
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var MongoClient = require("mongodb").MongoClient, assert = require("assert");
 var url = "mongodb://localhost:27017/waifus";
 var bodyParser = require("body-parser");
 var app = express();
-
+ObjectId = require('mongodb').ObjectID;
 
 MongoClient.connect(url, function (err, db) {
 	assert.equal(null, err);
@@ -14,46 +14,32 @@ MongoClient.connect(url, function (err, db) {
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-/*
-var waifus = [
-		["Satsuki Kiryuin", "Satsuki Kiryuin"],
-		["Maki Nishikino", "https://myanimelist.cdn-dena.com/images/characters/9/196139.jpg"],
-		["Papi", "https://pbs.twimg.com/profile_images/646956793650765824/imehwa-j.png"],
-		["Misaki Nakahara", "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/misaki-nakahara-welcome-to-the-nhk-37.2.jpg"],
-		["Chika Takami", "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/chika-takami--98.7.jpg"]
-	];
 
-	db.waifus.insert({name: "Maki Nishikino", imageAddress: "https://myanimelist.cdn-dena.com/images/characters/9/196139.jpg"});
-*/
 app.get("/waifus", function (req, res, next) {
 	MongoClient.connect(url, function (err, db) {
 		assert.equal(null, err);
-		console.log("Connection succesful inside app.get!");
 		var waifuCollection = db.collection('waifus');
-		var numberOfWaifus = waifuCollection.count();
 		waifuCollection.find({}).toArray(function (err, waifus) {
     		assert.equal(err, null);
-    		console.log("numberOfWaifus = " + numberOfWaifus);
-    		console.log("Found the following waifus");
-    		console.log("waifus: " + waifus[1].name);
-    		for (var i = 0; i < numberOfWaifus; i++) {
-    			console.log(waifus[i].name + " ")
-    		}
     		res.json(waifus);
     		db.close();
   		});
 	});
-	//res.send(waifus);
 });
 
 app.post("/waifus", function (req, res, next) {
-	/*
-	waifus.push([req.body.newWaifuName, req.body.newWaifuPic]);
-	res.send();
-	*/
 	MongoClient.connect(url, function(err, db) {
   		assert.equal(null, err);
   		postWaifu (db, req.body.newWaifuName, req.body.newWaifuPic, function() {
+      		db.close();
+  		});
+	});
+});
+
+app.put("/waifus/delete", function (req, res, next) {
+	MongoClient.connect(url, function(err, db) {
+  		assert.equal(null, err);
+  		deleteWaifu (db, req.body.waifuToDeleteId, function() {
       		db.close();
   		});
 	});
@@ -68,8 +54,20 @@ var postWaifu = function (db, name, imageAddress, callback) {
 		{"name" : name, "imageAddress" : imageAddress},
 		function(err, result) {
     		assert.equal(err, null);
-    		console.log("Inserted a waifu into the waifus collection.");
+    		console.log("Inserted waifu into the waifus collection.");
     		callback();
   		}
   	);
+}
+
+var deleteWaifu = function (db, id, callback) {
+	var idObject = ObjectId(id);
+	db.collection('waifus').deleteOne(
+		{"_id" : idObject},
+		function(err, result) {
+    		assert.equal(err, null);
+    		console.log("Removed waifu from collection.");
+    		callback();
+  		}
+    );
 }
