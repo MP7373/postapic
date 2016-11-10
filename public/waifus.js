7e34bd741580e706a9026d595233a8844bce2fba
MP7373 <martinpayne73@gmail.com>
2016-11-10 13:27:46 -0500

    Added sign up and login functionallity and comments

--- public/waifus.js
+++ public/waifus.js
@@ -1,23 +1,76 @@
-var app = angular.module("waifus", []);
+//initialize app
+var app = angular.module("waifus", ["ngRoute"]);
 
-app.controller("waifusController", function ($scope, $http) {
+//maps proper html files and controllers to different routes
+app.config(function ($routeProvider, $locationProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: "waifuFeed.html",
+		controller: "waifuFeedController"
+	})
+	.when("/signup", {
+		templateUrl: "signUp.html",
+		controller: "signUpController"
+	})
+});
+
+//controller for the home page that includes the main feed of waifus
+app.controller("waifuFeedController", function ($scope, $http) {
+
+	//loads feed with data upon first opening the page
 	updateWaifusFeed();
 
+	//signs user into account
+	$scope.signIn = function () {
+		$http.put("/userAccounts/signIn", {username: $scope.username, password: $scope.password}).then(function () {
+			alert("signed in");
+			$scope.username = "";
+			$scope.password = "";
+			updateWaifusFeed();
+		}, function (err) {
+			alert("bad username or password or both");
+			$scope.username = "";
+			$scope.password = "";
+			updateWaifusFeed();
+		});
+	};
+
+	//sends new waifu to database
 	$scope.submitWaifu = function () {
-		$http.post("/waifus", {newWaifuName: $scope.newWaifuName, newWaifuPic: $scope.newWaifuPic});
-		$scope.newWaifuName = "";
-		$scope.newWaifuPic = "";
-		updateWaifusFeed();
+		$http.post("/waifus", {newWaifuName: $scope.newWaifuName, newWaifuPic: $scope.newWaifuPic}).then(function () {
+			$scope.newWaifuName = "";
+			$scope.newWaifuPic = "";
+			updateWaifusFeed();
+		});
 	}
 	
+	//deletes waifu from database
 	$scope.deleteWaifu = function (waifu) {
-		$http.put("/waifus/delete", {waifuToDeleteId: waifu._id});
-		updateWaifusFeed();
+		$http.put("/waifus/delete", {waifuToDeleteId: waifu._id}).then(function () {
+			updateWaifusFeed();
+		});
 	}
 
+	//calls to update feed when page is loaded or database is changed
 	function updateWaifusFeed () {
-		$http.get("/waifus").then(function(response) {
+		$http.get("/waifus").then(function (response) {
 			$scope.waifus = response.data;
+		});
+	}
+});
+
+//controller for sign up for new account page
+app.controller("signUpController", function ($scope, $http) {
+
+	//sends new account to database
+	$scope.submitNewUserAccount = function () {
+		var newUserAccount = {
+			username: $scope.newUsername,
+			password: $scope.newPassword
+		};
+		$http.post("/userAccounts", newUserAccount).then(function () {
+			$scope.newUsername = "";
+			$scope.newPassword = "";
 		});
 	}
 });
--- server.js
+++ server.js
@@ -2,23 +2,26 @@ var express = require("express");
 var MongoClient = require("mongodb").MongoClient, assert = require("assert");
 var url = "mongodb://localhost:27017/waifus";
 var bodyParser = require("body-parser");
+var bcrypt = require("bcryptjs");
 var app = express();
-ObjectId = require('mongodb').ObjectID;
+app.use(bodyParser.json());
+app.use(express.static("public"));
+ObjectId = require("mongodb").ObjectID;
 
+//Check Mongodb connection
 MongoClient.connect(url, function (err, db) {
 	assert.equal(null, err);
 	console.log("Connection succesful!");
 	db.close();
 });
 
-app.use(bodyParser.json());
 
-app.use(express.static("public"));
 
+//Get waifus data from database
 app.get("/waifus", function (req, res, next) {
 	MongoClient.connect(url, function (err, db) {
 		assert.equal(null, err);
-		var waifuCollection = db.collection('waifus');
+		var waifuCollection = db.collection("waifus");
 		waifuCollection.find({}).toArray(function (err, waifus) {
     		assert.equal(err, null);
     		res.json(waifus);
@@ -27,32 +30,63 @@ app.get("/waifus", function (req, res, next) {
 	});
 });
 
+//calls postWaifu function to add a new waifu to database
 app.post("/waifus", function (req, res, next) {
 	MongoClient.connect(url, function(err, db) {
   		assert.equal(null, err);
-  		postWaifu (db, req.body.newWaifuName, req.body.newWaifuPic, function() {
+  		postWaifu(db, req.body.newWaifuName, req.body.newWaifuPic, function() {
       		db.close();
   		});
 	});
 });
 
+//calls deleteWaifu function to delete a waifu from database
 app.put("/waifus/delete", function (req, res, next) {
 	MongoClient.connect(url, function(err, db) {
   		assert.equal(null, err);
-  		deleteWaifu (db, req.body.waifuToDeleteId, function() {
+  		deleteWaifu(db, req.body.waifuToDeleteId, function() {
       		db.close();
   		});
 	});
 });
 
+//calls submitNewAccount function to add a new user account to database
+app.post("/userAccounts", function (req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      submitNewAccount(db, req.body.username, req.body.password, function() {
+          db.close();
+      });
+  });
+});
+
+//checks if inputted username and pasword are in database and if they are logs into that account
+app.put("/userAccounts/signIn", function (req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      userSignIn(db, req.body.username, req.body.password, 
+        function () {
+          db.close();
+          return res.send();
+        },
+        function () {
+          db.close();
+          return res.status(400).send();
+        }
+      );
+  });
+});
+
+//Gives port for app to listen on
 app.listen(3000, function () {
 	console.log("App listening on port 3000.");
 });
 
+//function that posts new waifu into database
 var postWaifu = function (db, name, imageAddress, callback) {
-	db.collection('waifus').insertOne(
+	db.collection("waifus").insertOne(
 		{"name" : name, "imageAddress" : imageAddress},
-		function(err, result) {
+		function (err, result) {
     		assert.equal(err, null);
     		console.log("Inserted waifu into the waifus collection.");
     		callback();
@@ -60,9 +94,10 @@ var postWaifu = function (db, name, imageAddress, callback) {
   	);
 }
 
+//function that deletes a selected waifu from the database
 var deleteWaifu = function (db, id, callback) {
 	var idObject = ObjectId(id);
-	db.collection('waifus').deleteOne(
+	db.collection("waifus").deleteOne(
 		{"_id" : idObject},
 		function(err, result) {
     		assert.equal(err, null);
@@ -70,4 +105,36 @@ var deleteWaifu = function (db, id, callback) {
     		callback();
   		}
     );
+}
+
+//function that submits a new user account to the database
+var submitNewAccount = function (db, username, password, callback) {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      db.collection("userAccounts").insertOne(
+        {"username" : username, "password" : hash},
+        function (err, submittedAccount) {
+          assert.equal(err, null);
+          console.log("Added new user account into the userAccounts collection.");
+          callback();
+        }
+      );
+    })
+  });
+}
+
+//function that checks users entered credentials are valid and if they are signs user in
+var userSignIn = function (db, username, password, validCallback, invalidCallback) {
+  db.collection("userAccounts").findOne(
+    {username: username},
+    function (err, selectedAccount) {
+      bcrypt.compare(password, selectedAccount.password, function (err, result) {
+        if (result) {
+          validCallback();
+        } else {
+          invalidCallback();
+        }
+      })
+    }
+  );
 }
